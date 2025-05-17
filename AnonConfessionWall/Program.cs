cbf7a7a62485c597a9c761525d72c3ceb9dc25ca
TwittK <koitristan123@gmail.com>
2025-05-17 18:07:22 +0000

    dotnet 8.0

--- AnonConfessionWall/Program.cs
+++ AnonConfessionWall/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,13 +18,13 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();       // Serve files from wwwroot
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+// app.MapStaticAssets();
+app.MapRazorPages();
 
 app.Run();
