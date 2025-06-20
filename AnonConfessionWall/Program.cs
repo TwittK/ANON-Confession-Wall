327166737eac968b3077eafb1cabd24e80e6a0c5
mortee <sherwinchan7227@gmail.com>
2025-06-20 12:40:53 +0800

    [Update]: Update port from 80 to 5000

--- AnonConfessionWall/Program.cs
+++ AnonConfessionWall/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:80");
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
